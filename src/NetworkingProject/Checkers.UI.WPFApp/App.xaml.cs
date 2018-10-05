ff2f22133ed796d746f8b76d2bba876c72606681
davdevor <drdcustomlows@gmail.com>
2018-10-05 19:28:55 -0400

    add wpf project for checkers

---
+++ src/NetworkingProject/Checkers.UI.WPFApp/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Checkers.Domain.Interfaces;
+
+namespace Checkers.UI.WPFApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            
+            MainWindow mainWindow = new MainWindow(DependencyInjectionContainer.Get<ICheckersRepository>());
+            mainWindow.Show();
+        }
+    }
+}
---
+++ src/NetworkingProject/Checkers.UI.WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Checkers.Domain.Interfaces;
+using System.Net.Http;
+
+namespace Checkers.UI.WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int[][] _gameState;
+        private int _playerId;
+        private ICheckersRepository _repository;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public MainWindow(ICheckersRepository checkersRepository)
+        {
+            _repository = checkersRepository;
+            InitializeComponent();
+        }
+
+        private async void StartGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            _playerId = await _repository.GetPlayerIdAsync();
+            _gameState = await _repository.GetGameStateAsync();
+            UIElementCollection gridChilds = this.CheckersGrid.Children;
+            for (int i = 0; i < _gameState.Length; ++i)
+            {
+                for (int j = 0; j < _gameState.Length; ++j)
+                {
+                    Button b = new Button();
+                    if (_gameState[i][j] == 1)
+                    {
+                        b.Content = "R";
+                    }
+                    else if (_gameState[i][j] == 2)
+                    {
+                        b.Content = "B";
+
+                    }
+                    gridChilds.Add(b);
+                    Grid.SetRow(b, i);
+                    Grid.SetColumn(b, j);
+                }
+            }
+            (sender as Button).Visibility = Visibility.Hidden;
+        }
+    }
+}
